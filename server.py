1c98b613f3a1cbb7a63b130b45350f1413e785ea
Nathan Steinway <steinway.nathan@gmail.com>
2023-08-24 17:40:05 -0500

    completes basic templating and url connections

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from model import connect_to_db, db
+from model import connect_to_db, db, User, Items, User_Items
 import os
 
 app = Flask(__name__)
@@ -9,6 +9,23 @@ app.secret_key = os.environ["POSTGRES_URI"]
 @app.route("/")
 def home():
     return render_template("base.html")
+
+@app.route("/hideout")
+def hideout():
+    hardware = Items.query.filter_by(item_category='Hardware')
+    return render_template("hideout.html", hardware=hardware)
+
+@app.route("/quests")
+def quests():
+    return render_template("quests.html")
+
+@app.route("/ammo")
+def ammo():
+    return render_template("ammo.html")
+
+@app.route("/armor")
+def armor():
+    return render_template("armor.html")
 
 if __name__ == "__main__":
     connect_to_db(app)
